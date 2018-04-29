a5bd3de672edbcd1bee50525c304df81bac4d95f
rogzam <rogzam@gmail.com>
2018-04-29 17:31:30 +0200

    Copied last version.
    Ended up copying what I had from skynet 2.3.2

--- master/master.ino
+++ master/master.ino
@@ -1,24 +1,31 @@
 /**
- * Marlin 3D Printer Firmware
- * Copyright (C) 2016 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
- *
- * Based on Sprinter and grbl.
- * Copyright (C) 2011 Camiel Gubbels / Erik van der Zalm
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * test edit from atom
+Skynet V2.3.2 edited for Penny
+
+April 29, 2018
+
+Edits:
+
+1 - On Configuration.h changend default steps per units as:
+
+    #define DEFAULT_AXIS_STEPS_PER_UNIT   {80,  80, 400, 95}
+    Also changed
+
+2 - On Version.h changed
+
+    Machine Name
+    Build config
+
+3 - On Configration_adv.h changed default thermal runway settings:
+
+    THERMAL_PROTECTION_PERIOD 60 to 100
+    THERMAL_PROTECTION_HYSTERESIS 10 to 4
+    WATCH_TEMP_PERIOD 20 to 40
+    WATCH_TEMP_INCREASE 2 to 3
+    THERMAL_PROTECTION_BED_PERIOD 60 to 150
+    THERMAL_PROTECTION_BED_HYSTERESIS 5 to 2
+    WATCH_BED_TEMP_PERIOD 180 to 90
+    WATCH_BED_TEMP_INCREASE 2  same
+
  */
 
 #include "MarlinConfig.h"
